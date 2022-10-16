f9e582b43cb3e90fc86104c8efa0a5025680f44e
nikolaymarkov3 <i@nikomar.ru>
2022-10-16 22:17:16 +0700

    My first project

--- src/main/java/firstJavaProject.java
+++ src/main/java/firstJavaProject.java
@@ -4,7 +4,7 @@ public class firstJavaProject {
     public static void main(String[] args) {
 
         System.out.println("Hi");
-        System.out.println("Привет мир");
+        System.out.println("Привет мир!");
 
 
 
