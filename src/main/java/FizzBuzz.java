093fd4e0950c2b626b8c0d52fcc92146e71cabe4
nikolaymarkov3 <i@nikomar.ru>
2022-10-20 22:46:37 +0700

    My first project

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,27 @@
+public class FizzBuzz {
+
+    public String[] fizzBuzz(int start, int end) {
+
+        if (start <= end) {
+            String[] array = new String[end - start + 1];
+            for (int i = 0; i <= array.length && start <= end; i++) {
+                if (start % 15 == 0) {
+                    array[i] ="FizzBuzz";
+                    return array;
+                }else if (start % 3 == 0){
+                    array[i] ="Fizz";
+                }else if (start % 5 == 0){
+                    array[i] ="Buzz";
+                }else {
+                    array[i] = String.valueOf(start);
+                }
+
+                start ++;
+            }
+        }
+
+        return new String[0];
+    }
+
+
+}
