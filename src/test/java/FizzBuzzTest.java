5e6471653249fb6aa34693c4c2dbc44251dbf09f
nikolaymarkov3 <i@nikomar.ru>
2022-10-21 17:06:01 +0700

    My first test

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,4 +1,5 @@
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class FizzBuzzTest {
@@ -36,17 +37,97 @@ public class FizzBuzzTest {
 
 
 
-
-
-
-
-
     //start == end
+
+    @Test
+
+    public void testStartEqualsEnd_HappyPath() {
+        //AAA
+        //arrange
+        int start = 1;
+        int end = 1;
+        String[] expectedResult = {"1"};
+
+        //act
+
+//        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String[] actualResult = new FizzBuzz().fizzBuzz(start,end);
+
+        //Assert
+
+        Assert.assertEquals(actualResult,expectedResult);
+
+    }
+
+    @Test
+    @Ignore
+
+    public void testStartLessThanEnd_StartAreNegativeHappyPath() {
+        //AAA
+        //arrange
+        int start = - 20;
+        int end = - 1;
+        String[] expectedResult = {"1", "2", "Fizz","4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13",
+                "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz"};
+
+        //act
+
+//        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String[] actualResult = new FizzBuzz().fizzBuzz(start,end);
+
+        //Assert
+
+        Assert.assertEquals(actualResult,expectedResult);
+
+    }
+
 
     /**2. Negative testing
      * if (start > end)
      * return new String[0];
      */
+
+    @Test
+
+    public void testStartGreaterThanEnt_Negative() {
+        //AAA
+        //arrange
+        int start = 20;
+        int end = 1;
+        String[] expectedResult = {};
+
+        //act
+
+//        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String[] actualResult = new FizzBuzz().fizzBuzz(start,end);
+
+        //Assert
+
+        Assert.assertEquals(actualResult,expectedResult);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
