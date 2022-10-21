59df01043fdbf531c89f7a497ac2c383b5f8469b
nikolaymarkov3 <i@nikomar.ru>
2022-10-21 16:11:16 +0700

    My first test

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -4,10 +4,12 @@ public class FizzBuzz {
 
         if (start <= end) {
             String[] array = new String[end - start + 1];
-            for (int i = 0; i <= array.length && start <= end; i++) {
+
+
+            for (int i = 0; i < array.length && start <= end; i++) {
                 if (start % 15 == 0) {
                     array[i] ="FizzBuzz";
-                    return array;
+
                 }else if (start % 3 == 0){
                     array[i] ="Fizz";
                 }else if (start % 5 == 0){
@@ -18,10 +20,66 @@ public class FizzBuzz {
 
                 start ++;
             }
+
+            return array;
         }
 
         return new String[0];
     }
+
+    /**вариант 2
+     *
+     * public String[] fizzBuzz(int start, int end) {
+
+     if (start <= end) {
+     String[] array = new String[end - start + 1];
+
+     int number = start;
+
+     for (int i = 0; i <= array.length && number <= end; i++) {
+     if (number % 15 == 0) {
+     array[i] ="FizzBuzz";
+     return array;
+     }else if (number % 3 == 0){
+     array[i] ="Fizz";
+     }else if (number % 5 == 0){
+     array[i] ="Buzz";
+     }else {
+     array[i] = String.valueOf(number);
+     }
+
+     number ++;
+     }
+
+     return array;
+     }
+
+     return new String[0];
+     }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,52 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class FizzBuzzTest {
+
+    /**1. Positive testing Happy path
+     * if (start <= end)
+     * return array;
+     */
+
+
+
+
+    //start < end
+    @Test
+
+    public void testStartLessThanEnd_HappyPath() {
+        //AAA
+        //arrange
+        int start = 1;
+        int end = 20;
+        String[] expectedResult = {"1", "2", "Fizz","4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13",
+                                    "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz"};
+
+        //act
+
+//        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String[] actualResult = new FizzBuzz().fizzBuzz(start,end);
+
+        //Assert
+
+        Assert.assertEquals(actualResult,expectedResult);
+
+    }
+
+
+
+
+
+
+
+
+    //start == end
+
+    /**2. Negative testing
+     * if (start > end)
+     * return new String[0];
+     */
+
+
 }
